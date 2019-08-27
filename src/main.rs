53d8f660eede0f9256c5ed760456e55210545e88
Simon Struck <simon@simonscode.org>
2019-08-27 20:41:57 +0200

    Add arguments and improve diffing

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,54 @@
-use std::fs::File;
+use std::fs::{File, read_dir};
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: {} source destination", args[0]);
+        return;
+    }
+    let src = &args[1];
+    let dst = &args[2];
     println!("SRC -> DST:");
-    cascade("/data/media", "/mnt/media");
+    cascade(src, dst, true);
     println!();
     println!();
     println!();
     println!();
     println!("DST -> SRC:");
-    cascade("/mnt/media", "/data/media");
+    // no need to compare sizes on the second pass, as we're only interested in the files that exist in the destination and not in the source
+    cascade(dst, src, false);
 }
 
-fn cascade(src: &str, dst: &str) {
-    let result = std::fs::read_dir(src);
+fn cascade(src: &str, dst: &str, compare_size: bool) {
+    let result = read_dir(src);
 
-    if result.is_err() {
-        let sfile = File::open(src).expect(&format!("Error reading src file: {}", src));
+    let dfile = File::open(dst);
 
-        let dfile = File::open(dst);
-        if dfile.is_err() {
-            println!("Error reading dst file: {}", dst);
-            return;
-        }
-        let dfile = dfile.unwrap();
-
-        let smeta = sfile.metadata().expect(&format!("Error reading src file metadata: {}", src));
-        let dmeta = dfile.metadata().expect(&format!("Error reading dst file metadata: {}", dst));
-
-        if smeta.len() != dmeta.len() {
-            println!("Mismatch len: {}\t{}\t{}", src, smeta.len(), dmeta.len());
-        }
+    if dfile.is_err() {
+        println!("File missing or unreadable: {}", dst);
         return;
     }
+    let dfile = dfile.unwrap();
 
-    for file in result.unwrap() {
-        let file = file.unwrap();
-        cascade(&format!("{}/{}", src, file.file_name().to_str().unwrap()), &format!("{}/{}", dst, file.file_name().to_str().unwrap()));
+    match result {
+        Ok(folder) => {
+            for file in folder {
+                let file = file.expect(&format!("Error unwrapping DirEntry in subdirectory of: {}", src));
+                cascade(&format!("{}/{}", src, file.file_name().to_str().unwrap()), &format!("{}/{}", dst, file.file_name().to_str().unwrap()), compare_size);
+            }
+        }
+        Err(_) => { // meaning it's a file and not a directory
+            if compare_size {
+                let sfile = File::open(src).expect(&format!("Error reading src file: {}", src));
+
+                let smeta = sfile.metadata().expect(&format!("Error reading src file metadata: {}", src));
+                let dmeta = dfile.metadata().expect(&format!("Error reading dst file metadata: {}", dst));
+
+                if smeta.len() != dmeta.len() {
+                    println!("Mismatch len: {}\t{}\t{}", src, smeta.len(), dmeta.len());
+                }
+            }
+        }
     }
 }
